5a6cbd86d45e9a596a4a6c495a8df653afd631b9
Nandhu <knandhu.27@gmail.com>
2020-02-01 16:34:38 -0800

    heroku error fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import win_logic from './game_logic';
 // const PORT = process.env.PORT || 5000;
-http.createServer(onRequest).listen(process.env.PORT || 5000);
+http.createServer(onRequest).listen(process.env.PORT || 6000);
 var worldRadius = 26;
 var pathAngleValues = [1.52, 1.57, 1.62];
 var treesPool = [];
